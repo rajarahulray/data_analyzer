0e46c6eaeaa71d16c9abc100cac433e75b1e0e74
rajarahulray <rajaray2208@gmail.com>
2019-02-07 20:21:34 +0530

    data_nalyzer_cli_package_with_dummy_cli_app.py_file

---
+++ data_analyzer_cli/cli_app.py
@@ -0,0 +1,24 @@
+# Structure for the cli app
+"""
+options for the cli :
+1. --file : to_input file (.csv, .xls, .xlsx) only.
+2. --operation : 
+    1. analyze : give summary about the file with all the rows and column counts,
+                mean, median, mode of each column and overall data.
+    2. analyzewithgraph : performs the analysis(like the above operation), and outputs
+                            the result in a visual graph.
+"""
+import pandas
+
+
+class DataAnalyzerCli:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
